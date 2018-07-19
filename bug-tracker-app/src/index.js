73b89550c9c5d3353969f14d352e34f474b136e8
Magesh <tkmagesh77@gmail.com>
2018-07-19 17:27:50 +0530

    routing implementation

--- bug-tracker-app/src/index.js
+++ bug-tracker-app/src/index.js
@@ -6,8 +6,28 @@ import appStore from './store';
 import BugTracker from './bugTracker';
 import Spinner from './spinner';
 import './index.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 ReactDOM.render(
+	<Provider store={appStore}>
+		<Router>
+			<div>
+				<p>
+				<Link to="/">Bug Tracker</Link>
+				<br/>
+				<Link to="/spinner">Spinner</Link>
+				<br />
+				</p>
+			  <Route exact path="/" component={BugTracker} />
+		      <Route path="/bugs" component={BugTracker} />
+		      <Route path="/spinner" component={Spinner} />
+		    </div> 
+	    </Router>
+	</Provider>
+	, document.getElementById('root')
+);
+
+/*ReactDOM.render(
 <Provider store={appStore}>
 	<div>
 		<Spinner/>
@@ -17,7 +37,7 @@ ReactDOM.render(
 </Provider>
 ,
 document.getElementById('root'));
-
+*/
 
 
 
--- bug-tracker-app/src/store/middlewares/func.js
+++ bug-tracker-app/src/store/middlewares/func.js
@@ -3,7 +3,7 @@ function func(store){
 	return function(next){
 		return function(action){
 			if (typeof action === 'function'){
-				return action(store.getState(), store.dispatch);
+				return action(store.dispatch, store.getState);
 			}
 			return next(action);
 		}
